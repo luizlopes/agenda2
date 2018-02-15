cf0d5f46c81d3ce20ea8bb5933587e0328e1ae47
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-02-14 23:42:35 -0200

    extracting class Field

--- lib/active_file.rb
+++ lib/active_file.rb
@@ -5,8 +5,12 @@ module ActiveFile
     end
 
     module ClassMethods
+        attr_accessor :fields
+
         def field(name, required: false)
             puts "the field '#{name}' is #{required ? '' : 'not'} required"
+            @fields ||= []
+            @fields << Field.new(name, required)
 
             self.class_eval do
                 if required
@@ -15,6 +19,14 @@ module ActiveFile
                     attr_accessor name
                 end
             end
+        end
+    end
+
+    class Field
+        attr_reader :name, :required
+
+        def initialize(name, required) 
+            @name, @required = name, required
         end
     end
 
