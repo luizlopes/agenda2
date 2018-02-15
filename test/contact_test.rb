858b7d4e59ca768680cecdff1b4f4fbc4b23a0d8
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-02-15 20:21:30 -0200

    refactoring tests

--- test/contact_test.rb
+++ test/contact_test.rb
@@ -3,21 +3,26 @@ require 'contact'
 
 class ContactTest < MiniTest::Test
 
+    def setup
+        @contact = Contact.new
+    end
+
     def test_new_contact
-        contact = Contact.new
-        assert(contact, "contact is nil")
-        assert_respond_to(contact, :name)
-        refute_respond_to(contact, :name=)
-        assert_respond_to(contact, :phone)
-        assert_respond_to(contact, :phone=)
-        assert_respond_to(contact, :email)
-        assert_respond_to(contact, :email=)
+        assert(@contact, "contact is nil")
+        assert_respond_to(@contact, :phone)
+        assert_respond_to(@contact, :phone=)
+        assert_respond_to(@contact, :email)
+        assert_respond_to(@contact, :email=)
+    end
+
+    def test_name_field_reader
+        assert_respond_to(@contact, :name)
+        refute_respond_to(@contact, :name=)
     end
 
     def test_id_field_accessor
-        contact = Contact.new
-        assert_respond_to(contact, :id)
-        assert_respond_to(contact, :id=)
+        assert_respond_to(@contact, :id)
+        assert_respond_to(@contact, :id=)
     end
 
 end
