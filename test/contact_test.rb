5c4ea9f67cdec853eb369592c5626792686ff86c
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-02-15 20:22:47 -0200

    refactoring tests

--- test/contact_test.rb
+++ test/contact_test.rb
@@ -20,6 +20,16 @@ class ContactTest < MiniTest::Test
         refute_respond_to(@contact, :name=)
     end
 
+    def test_phone_field_accessor
+        assert_respond_to(@contact, :phone)
+        assert_respond_to(@contact, :phone=)
+    end
+
+    def test_email_field_accessor
+        assert_respond_to(@contact, :email)
+        assert_respond_to(@contact, :email=)
+    end
+
     def test_id_field_accessor
         assert_respond_to(@contact, :id)
         assert_respond_to(@contact, :id=)
