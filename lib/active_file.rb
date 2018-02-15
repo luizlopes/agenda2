9519e37b30c9d20cc69707e421aab1007f0dc5f9
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-02-14 22:59:23 -0200

    creating attributes accessors into Contact

--- lib/active_file.rb
+++ lib/active_file.rb
@@ -8,6 +8,10 @@ module ActiveFile
     module ClassMethods
         def field(name, required: false)
             puts "the field '#{name}' is #{required ? '' : 'not'} required"
+
+            self.class_eval do
+                attr_accessor name
+            end
         end
     end
 
--- test/contact_test.rb
+++ test/contact_test.rb
@@ -6,6 +6,9 @@ class ContactTest < MiniTest::Test
     def test_new_contact
         contact = Contact.new
         assert(contact, "contact is nil")
+        assert_respond_to(contact, :name)
+        assert_respond_to(contact, :phone)
+        assert_respond_to(contact, :email)
     end
 
 
